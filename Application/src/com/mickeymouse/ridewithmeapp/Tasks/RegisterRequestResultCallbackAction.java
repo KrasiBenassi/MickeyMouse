ec3ae7ce725e0be3e41b1709b2b59811f4e947d4
sophialazarova <sophia.lazarova@yahoo.com>
2014-10-15 13:14:05 +0300

    RegisterRequestResultCallbackAction

---
+++ Application/src/com/mickeymouse/ridewithmeapp/Tasks/RegisterRequestResultCallbackAction.java
@@ -0,0 +1,48 @@
+package com.mickeymouse.ridewithmeapp.Tasks;
+
+import android.R;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.mickeymouse.ridewithmeapp.Activities.EntryActivity;
+import com.mickeymouse.ridewithmeapp.Activities.RegisterActivity;
+import com.telerik.everlive.sdk.core.result.RequestResult;
+import com.telerik.everlive.sdk.core.result.RequestResultCallbackAction;
+
+public class RegisterRequestResultCallbackAction extends RequestResultCallbackAction {
+    private Activity activity;
+    private ProgressDialog progressDialog;
+
+    public RegisterRequestResultCallbackAction(RegisterActivity registerActivity, ProgressDialog registerProgressDialog) {
+        this.activity = registerActivity;
+        this.progressDialog = registerProgressDialog;
+    }
+
+    @Override
+    public void invoke(RequestResult requestResult) {
+        this.progressDialog.dismiss();
+
+        if (requestResult.getSuccess()) {
+
+            this.activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                	Intent intent = new Intent(activity, EntryActivity.class);
+                   Toast.makeText(activity, activity.getResources().getString(com.mickeymouse.ridewithmeapp.R.string.register_onsucceed), Toast.LENGTH_LONG).show();
+                   activity.startActivity(intent);
+                }
+            });
+        } else {
+            final String errorMessage = requestResult.getError().getMessage();
+
+            this.activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                	Toast.makeText(activity, errorMessage, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
